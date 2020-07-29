5d8c8c2d8a53238054a41358b9fc1ae9c6af54d6
felipe <juan.orozcoco@gmail.com>
2020-07-29 13:34:27 -0500

    recycle view horizontal

--- app/src/main/java/com/example/claficados/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/claficados/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.claficados.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +15,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.claficados.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     String url ="https://comcop.com.co/persia";
+    private static final String TAG = "MainActivity";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,7 +46,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-         */
+
 
         final WebView webMagento=root.findViewById(R.id.webMagento);
         webMagento.setWebViewClient(new MyWebViewClient());
@@ -44,16 +54,73 @@ public class HomeFragment extends Fragment {
         WebSettings settings=webMagento.getSettings();
         settings.setJavaScriptEnabled(true);
         webMagento.loadUrl(url);
+
+         */
+
+        getImages(root);
+
         return root;
 
 
 
     }
 
+    private void getImages(View root) {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mNames.add("Rocky Mountain National Park");
+
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
+        initRecyclerView(root);
+    }
+
+    private void initRecyclerView(View root) {
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        final RecyclerView recyclerView =root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+    }
+
+    }
+/*
     private class MyWebViewClient extends WebViewClient{
         public boolean shouldOverrideUrlLoading(WebView view, String url){
             view.loadUrl(url);
             return true;
         }
     }
-}
+
+ */
+
+
+
