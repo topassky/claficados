f819c7f2f81befff1bdd833849455645b52203fe
C.argoti <argotty2010@gmail.com>
2020-08-01 15:51:14 -0400

    importadas las clases de conexiones

---
+++ app/src/main/java/com/example/claficados/ConecionSQLiteHelper.java
@@ -0,0 +1,32 @@
+package com.example.claficados;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.claficados.oi.utilities;
+
+///import com.example.ciclista_actual.utilities.Utilities;
+
+public class ConecionSQLiteHelper extends SQLiteOpenHelper {
+
+    public ConecionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(utilities.createTable0);
+        db.execSQL(utilities.createTable1);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS  "+utilities.tblmuestra);
+        db.execSQL("DROP TABLE IF EXISTS  "+utilities.tblconfiguraciones);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/claficados/oi/utilities.java
+++ app/src/main/java/com/example/claficados/oi/utilities.java
@@ -1,19 +1,42 @@
 package com.example.claficados.oi;
 
 public class utilities {
+
+    public static String idMuestra = "id";
+    public static final String tblmuestra = "hojaVida";
+    public static final String nombreUsuario = "usuario";
+    public static final String fechainicio = "fechaInico";
+    public static final String fechafinal = "fechaFinal";
+    public static final String empresa = "Empresa";
+    public static final String nombrejefe = "nombreJefe";
+    public static final String teljefe = "telJefe";
+
+
+    public static final String createTable0 = "CREATE TABLE " + tblmuestra + " (" + idMuestra + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            nombreUsuario + " TEXT ," + fechainicio + " TEXT ," + fechafinal + " TEXT ," + empresa + " TEXT ," + nombrejefe +
+            " TEXT ," + teljefe + " TEXT)";
+
+
+    public static final String tblconfiguraciones = "hdgsttsus";
+    public static final String idconig = "jjchkc";
+
+    public static final String json = "json";
+
+    public static final String createTable1 = "CREATE TABLE " + tblconfiguraciones + " (" + idconig + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + json + " TEXT)";
+
+
     public static String[] urlCover={"http://www.comcop.com.co/persia/imgsApp/logoComcop.png",
             "https://i.redd.it/qn7f9oqu7o501.jpg",
             "https://i.redd.it/j6myfqglup501.jpg",
             "https://i.redd.it/0h2gm1ix6p501.jpg",
             "https://i.redd.it/k98uzl68eh501.jpg",
-            "http://www.comcop.com.co/persia/imgsApp/agregar.jpg"
+            "http://www.comcop.com.co/persia/imgsApp/agregar.jpg",
             "http://www.comcop.com.co/persia/imgsApp/agregar.jpg"
 
     };
 
-    public static String[] urlmNames={"Trondheim",
-            "Portugal",
-            "Rocky Mountain National Park",
+    public static String[] urlmNames={"Trondheim",            "Portugal",            "Rocky Mountain National Park",
             "Mahahual",
             "Frozen Lake",
             "Ver más empresas",
