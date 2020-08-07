ac97ab66d9f1438b084c1eb77ea418371b15786c
felipe <juan.orozcoco@gmail.com>
2020-08-06 20:21:08 -0500

    solucion al duplicado de recyclerview

--- app/src/main/java/com/example/claficados/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/claficados/ui/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,12 +16,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.claficados.R;
+import com.example.claficados.ui.thing.thing;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,11 +44,36 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
    // RadioButton r1,r0,r2,r3,r4,r5;
     TextView contador;
+    Button btnPrductos;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnPrductos = (Button)view.findViewById(R.id.Productos);
+        btnPrductos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"hola",Toast.LENGTH_SHORT).show();
+
+                Navigation.findNavController(view).navigate(R.id.thing);
+
+            }
+        });
+
+
+
+
+
         /*
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -72,8 +101,9 @@ public class HomeFragment extends Fragment {
         r5 = root.findViewById(R.id.radio5);
 
          */
-        recyclerView =(RecyclerView)root.findViewById(R.id.recyclerView);
-        contador = (TextView)root.findViewById(R.id.contador);
+        recyclerView =(RecyclerView)view.findViewById(R.id.recyclerView);
+        contador = (TextView)view.findViewById(R.id.contador);
+        contador.setText("1-"+urlCover.length);
 
 
 
@@ -121,28 +151,21 @@ public class HomeFragment extends Fragment {
 
         });
 
-
-        getImages(root);
-
-        return root;
-
-
-
+        mImageUrls.clear();
+        mNames.clear();
+        getImages(view);
     }
-
-
-
-
-
 
     private void getImages(View root) {
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
         for (int i=0;i<urlCover.length;i++){
-            mImageUrls.add(urlCover[i]);
-            mNames.add(urlmNames[i]);
+            mImageUrls.add(i,urlCover[i]);
+            mNames.add(i,urlmNames[i]);
 
         }
+
+
 
         initRecyclerView(root);
     }
--- app/src/main/java/com/example/claficados/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/claficados/ui/slideshow/SlideshowFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,12 +21,26 @@ public class SlideshowFragment extends Fragment {
 
 
 
+    String url ="https://comcop.com.co/persia";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
+        final WebView webMagento=root.findViewById(R.id.webMagento);
+        webMagento.setWebViewClient(new MyWebViewClient());
 
+        WebSettings settings=webMagento.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webMagento.loadUrl(url);
         return root;
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
