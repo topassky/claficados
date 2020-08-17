bab2489c7454f87eae38ef217bf3a36a50be407f
felipe <juan.orozcoco@gmail.com>
2020-08-17 14:52:36 -0500

    elegir foto desde galeria

--- app/src/main/java/com/example/claficados/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/claficados/ui/gallery/GalleryFragment.java
@@ -1,10 +1,24 @@
 package com.example.claficados.ui.gallery;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +28,25 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.claficados.R;
 
-public class GalleryFragment extends Fragment {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class GalleryFragment extends Fragment implements PopupMenu.OnMenuItemClickListener {
+
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp/";//directorio principal
+    private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
+    private String path;//almacena la ruta de la imagen
+    File fileImagen;
+    Bitmap bitmap;
+    Button btnFoto;
+
+    ImageView imgFoto;
+
+
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,4 +55,107 @@ public class GalleryFragment extends Fragment {
 
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnFoto = (Button)view.findViewById(R.id.btnFoto);
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(getContext(), view);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.menu_foto, popup.getMenu());
+                popup.setOnMenuItemClickListener(GalleryFragment.this);
+                popup.show();
+            }
+        });
+
+        imgFoto=(ImageView)view.findViewById(R.id.imgFoto);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.fotoGaleria:
+                abriGaleria();
+                return true;
+            case R.id.fotoCamara:
+                Toast.makeText(getContext(), "Estoy aqui"
+                        , Toast.LENGTH_LONG).show();
+                abriCamara();
+
+                return true;
+
+            default:
+                return false;
+        }
+
+    }
+
+    private void abriGaleria() {
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+    }
+
+    private void abriCamara() {
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if(isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if(isCreada==true){
+            Long consecutivo= System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;//indicamos la ruta de almacenamiento
+
+            fileImagen=new File(path);
+
+            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+
+
+            startActivityForResult(intent,COD_FOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case COD_SELECCIONA:
+                Uri miPath=data.getData();
+                imgFoto.setImageURI(miPath);
+
+                try {
+                    bitmap=MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),miPath);
+                    imgFoto.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();}
+
+                break;
+            case COD_FOTO:
+                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("Path",""+path);
+                            }
+                        });
+
+                bitmap= BitmapFactory.decodeFile(path);
+                imgFoto.setImageBitmap(bitmap);
+
+                break;
+        }
+    }
+
 }
